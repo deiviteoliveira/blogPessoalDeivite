83a8deacd4081c116f6e220ad0220a05c7c5e9ac
Deivite Oliveira <90745992+deiviteoliveira@users.noreply.github.com>
2021-12-06 21:49:13 -0300

    Implementação de consumo parte 1

--- blogPessoalDeivite/blogPessoalDeivite/src/app/cadastrar/cadastrar.component.ts
+++ blogPessoalDeivite/blogPessoalDeivite/src/app/cadastrar/cadastrar.component.ts
@@ -1,15 +1,59 @@
-import { Component, OnInit } from '@angular/core';
+  import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+  import { Usuario } from '../model/Usuario';
+  import { AuthService } from '../service/auth.service';
 
-@Component({
-  selector: 'app-cadastrar',
-  templateUrl: './cadastrar.component.html',
-  styleUrls: ['./cadastrar.component.css']
-})
-export class CadastrarComponent implements OnInit {
 
-  constructor() { }
+  @Component({
+    selector: 'app-cadastrar',
+    templateUrl: './cadastrar.component.html',
+    styleUrls: ['./cadastrar.component.css']
+  })
+  export class CadastrarComponent implements OnInit {
 
-  ngOnInit(): void {
+    usuario: Usuario = new Usuario
+    confirmarSenha: string
+    tipoUsuar: string
+
+    constructor(
+
+      private authService: AuthService,
+      private router: Router
+    ) { }
+
+    ngOnInit() {
+      window.scroll(0, 0)
+
+
+    }
+    confirmSenha(event: any) {
+      this.confirmarSenha = event.target.value
+
+    }
+
+    tipoUsuario(event: any) {
+      this.tipoUsuar = event.target.value
+
+
+    }
+    cadastrar() {
+      this.usuario.tipo = this.tipoUsuar
+
+      if (this.usuario.senha != this.confirmarSenha) {
+        alert('As senhas estão incorretas.')
+
+      } else {
+
+        this.authService.cadastrar(this.usuario).subscribe ((resp: Usuario) => {
+        this.usuario = resp
+        this.router.navigate(['/entrar'])
+        alert('Usuário cadastrado com sucesso!')
+
+        })
+
+      }
+    }
+
+
+
   }
-
-}
